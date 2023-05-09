4579147409c3b84e8418e2c3b00eb4d1994371fd
Deekshitha <ag.deekkshitha@gmail.com>
2023-05-09 21:13:44 +0530

    eda and prob statement

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPHEN_E_DOT='-e .'
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
-        '''
-        this function returns a list of libraries
-        '''
-        requirements=[]
-        with open(file_path) as file_obj:
-            requirements=file_obj.readlines()
-            requirements=[req.replace("\n","") for req in requirements]
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-            if HYPHEN_E_DOT in requirements:
-                requirements.remove(HYPHEN_E_DOT)
-        return requirements
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
 setup(
 name='mlproject',
 version='0.0.1',
@@ -21,4 +23,5 @@ author='Deekkshitha',
 author_email='ag.deekkshitha@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
+
 )
